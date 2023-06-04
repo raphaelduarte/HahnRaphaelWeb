986c6f6799cedb4421f25f1c1d39d96c95d1f4b8
Raphael Duarte <raphaelduartessph@gmail.com>
2023-06-04 09:59:13 -0300

    Creating the product entity.

--- HahnRaphaelWeb.Domain/Entities/Entity.cs
+++ HahnRaphaelWeb.Domain/Entities/Entity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HahnRaphaelWeb.Domain.Entities
+﻿namespace HahnRaphaelWeb.Domain.Entities
 {
     public abstract class Entity : IEquatable<Entity>
     {
---
+++ HahnRaphaelWeb.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace HahnRaphaelWeb.Domain.Entities
+{
+    public class Product : Entity
+    {
+        public Product(string name, string description, double price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public double Price { get; private set; }
+    }
+}
