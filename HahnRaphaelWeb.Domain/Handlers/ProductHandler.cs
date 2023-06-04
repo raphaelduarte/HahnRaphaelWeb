ef89f66435e9cd4299bb349493163019c0543f5b
Raphael Duarte <raphaelduartessph@gmail.com>
2023-06-04 12:27:48 -0300

    Fail fast validation.

---
+++ HahnRaphaelWeb.Domain/Handlers/ProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation.Results;
+using HahnRaphaelWeb.Domain.Commands;
+using HahnRaphaelWeb.Domain.Commands.Contracts;
+using HahnRaphaelWeb.Domain.Entities;
+using HahnRaphaelWeb.Domain.Handlers.Contracts;
+using HahnRaphaelWeb.Domain.Repositories;
+
+namespace HahnRaphaelWeb.Domain.Handlers
+{
+    public class ProductHandler : IHandler<CreateProductCommand>, IHandler<UpdateProductCommand>
+    {
+        private readonly IProductRepository _repository;
+        public ProductHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        public ICommandResult Handle(CreateProductCommand command)
+        {
+            if (!command.Validate(command).IsValid)
+            {
+                return new GenericCommandResult(false, "Ops, it is not valid", command);
+            };
+            
+            
+        }
+
+        public ICommandResult Handle(UpdateProductCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
